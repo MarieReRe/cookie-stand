a5062407c9afda5dcebefdb4b43066fa7546e5fa
Marie Antons <maantons@mail.fhsu.edu>
2020-03-02 14:06:10 -0600

    added js scaffolding

--- js/app.js
+++ js/app.js
@@ -1,5 +1,29 @@
 'use strict';
 console.log('Hey these salmon cookies are pretty good.');
 
+var seattleLocation = {
+    customers: 
+    this.customers =
+    cookies: 
+};
 
+var tokyoLocation = {
+    customers:
+    this.customers = 
+};
+
+var dubaiLocation = {
+    customers:
+};
+var parisLocation = {
+    customers:
+};
+
+var limaLocation = {
+    customers:
+};
+
+function randomCookies(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
