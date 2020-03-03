ec0bc07a55a144ddce773dd8685220a4fbbd25f7
Marie Antons <maantons@mail.fhsu.edu>
2020-03-03 15:01:44 -0600

    added cell data to rows

--- app2.js
+++ app2.js
@@ -1,4 +1,13 @@
-function Store(minCustomers, maxCustomers, avgCookies) {
+'use strict';
+console.log('Hey I work');
+
+var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+function randomCustomers(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+function Store(storeName, minCustomers, maxCustomers, avgCookies) {
+    this.storeName = storeName;
     this.minCustomers = minCustomers;
     this.maxCustomers = maxCustomers;
     this.avgCookies = avgCookies;
@@ -9,32 +18,54 @@ Store.prototype.setCustomers = function () {
     var total = 0;
     for (var i = 0; i < hours.length; i++) {
         var hourly = Math.floor(randomCustomers(this.minCustomers, this.maxCustomers) * this.avgCookies);
-        this.cookies.push(`${hours[i]}: ${hourly}`);
+        this.cookies.push(hourly);
         total += hourly;
     }
-    this.cookies.push('total: ' + total);
+    this.cookies.push(total);
     console.log(this.cookies);
 },
 
-//DOM elements to render on the page
+    //DOM elements to render on the page
 
-Store.prototype.render = function() {
-console.log(this.minCustomers);
-    
-};
+    Store.prototype.render = function () {
+        console.log(this.minCustomers);
+        var storeTable = document.getElementById('cookie-data');
+        var storeRow = document.createElement('tr');
+        var nameCell = document.createElement('td');
+        nameCell.textContent = this.storeName;
+        storeRow.appendChild(nameCell);
 
-var seattle = new Store(23, 65, 6.3);
-var tokyo = new Store(3, 24,1.2);
-var dubai = new Store(11, 38,3.7);
-var paris = new Store(20,38,2.3);
-var lima = new Store(2, 16, 4.6);
+        for (var i = 0; i < this.cookies.length; i++) {
+            var cookieCell = document.createElement('td');
+            cookieCell.textContent = this.cookies[i];
+            storeRow.appendChild(cookieCell);
+        }
 
-var storeLocations =[seattle, tokyo, dubai, paris, lima];
- //loop through objects
- for(var i = 0; i < storeLocations.length; i++){
-     storeLocations[i].render();
- }
- console.log(storeLocations);
+       
+        storeTable.appendChild(storeRow);
+
+
+    };
+
+var seattle = new Store('seattle', 23, 65, 6.3);
+var tokyo = new Store('tokyo', 3, 24, 1.2);
+var dubai = new Store('dubai', 11, 38, 3.7);
+var paris = new Store('paris', 20, 38, 2.3);
+var lima = new Store('lima', 2, 16, 4.6);
+
+var storeLocations = [seattle, tokyo, dubai, paris, lima];
+// //adding the hours to the top of the table
+// for (var i = 0; i < hours.length; i++) {
+//     var hoursCell = document.createElement('tr');
+//     hoursCell.textContent = hours[i];
+//     storeRow.appendChild(hoursCell);
+// }
+//loop through objects
+for (var i = 0; i < storeLocations.length; i++) {
+    storeLocations[i].setCustomers();
+    storeLocations[i].render();
+}
+console.log(storeLocations);
 //seattle.render();
 
 
